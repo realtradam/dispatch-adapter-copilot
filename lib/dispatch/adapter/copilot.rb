b13ec1794c81a70b457df70c0b34a32d94dc36b8
Adam Malczewski <github@tradam.dev>
2026-03-31 19:05:55 +0900

    init

---
+++ lib/dispatch/adapter/copilot.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "copilot/version"
+
+module Dispatch
+  module Adapter
+    module Copilot
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/dispatch/adapter/copilot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dispatch::Adapter::Copilot do
+  it "has a version number" do
+    expect(Dispatch::Adapter::Copilot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
